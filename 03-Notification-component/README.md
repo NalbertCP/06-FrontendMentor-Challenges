d74b69d038422fff665f375b62c593addf2990c7
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-10-03 19:19:55 -0300

    Add files via upload

---
+++ 03-Notification-component/README.md
@@ -0,0 +1,35 @@
+<h1>👾 Notification component</h1>
+<div class="badges">
+  <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white">
+  <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white">
+  <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E">
+</div>
+
+<h2>📋 Descrição</h2>
+<p>
+    Uma página de notificações criada com HTML, CSS e Java Script. Projeto feito a partir do design fornecido
+    em um dos desafios encontrados na página Front-End Mentor. O componente possui as seguintes funcionalidades:
+</p>
+<ul>
+  <li>Diferenciação entre notificações lidas e não lidas;</li>
+  <li>Botao para marcar todas as notificações como lidas;</li>
+  <li>Contador de notifcações não lidas.</li>
+</ul>
+<p><b>O design do projeto original fornecido pela Front-End Metor segue abaixo:</b></p>
+
+```md
+[Notification component challenge] https://www.frontendmentor.io/challenges/notifications-page-DqK5QAmKbC
+```
+
+<img width="800px" src="https://user-images.githubusercontent.com/105606295/193695631-206cccfe-66d5-4ec3-8edc-c54254549b75.png">
+
+
+<h2> 💻 Stacks utilizadas</h2>
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>Java Script</li>
+</ul>
+
+<h2> 💵 Licença</h2>
+<p><b>O uso de todos os arquivos deste projeto é limitado apenas a fins de estudos.<b></p>
---
+++ 03-Notification-component/assets/js/script.js
@@ -0,0 +1,43 @@
+/*Declarando variáveis*/
+const notifications = document.querySelectorAll(".notification")
+const noteCount = document.querySelector(".notifications-count")
+const elementsClass = ["profile-name", "additional-content", "note-img"]
+
+/*Adicionando event listeners aos elementos*/
+window.addEventListener("click",(event)=>{
+    if (event.target.className === "mark-as-read") readAll()
+})
+for (let notefication of notifications){
+    notefication.addEventListener("click", notificationEvent)
+}
+
+/*Funções*/
+function notificationEvent(event){
+    let notification = this
+    let targetClass = event.target.className
+
+    if (! elementsClass.every((value)=> value!=targetClass)){
+        if (notification.classList[2]==="read") return event.stopPropagation()
+        else return changeNotificationCount(notification)
+    }
+
+    changeNotificationCount(notification)
+}
+function changeNotificationCount(notification){
+    markNotification(notification)
+    if (notification.classList[2]==="read") noteCount.innerHTML = Number(noteCount.innerHTML) - 1
+    else noteCount.innerHTML = Number(noteCount.innerHTML) + 1
+}
+function markNotification(notification){
+    notification.classList.toggle("read")
+    notification.querySelector(".unread-marker").classList.toggle("hidden")
+}
+
+function readAll(){
+    for (let notification of notifications){
+        notification.classList.add("read")
+        notification.querySelector(".unread-marker").classList.add("hidden")
+    }
+    noteCount.innerHTML = 0
+}
+

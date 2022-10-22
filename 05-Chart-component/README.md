96a682074a87340b12bff2dce52bda01aa2d6e70
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-10-22 20:19:22 -0300

    Add files via upload

---
+++ 05-Chart-component/README.md
@@ -0,0 +1,31 @@
+<h1>💰 Expenses chart component</h1>
+<div class="badges">
+  <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white">
+  <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white">
+  <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E">
+</div>
+<h2>📋 Descrição</h2>
+<p>
+    Projeto criado a partir de um dos desafios disponíveis na página Front-end Mentor.
+    O projeto teve como objetivo criar um componente para exibir um resumo das despesas semanais do usuário em um gráfico de barras. Dentre as funcionalidades implementadas estão:
+<p>
+<ul>
+  <li>Filtragem do resumo de gastos por semana</li>
+  <li>Visualização do gasto diário em cada barra do gráfico</li>
+</ul>
+
+<p><b>O design do projeto original fornecido pela <a href="https://www.frontendmentor.io/" target="_blank">Front-end Mentor</a> segue no link abaixo:</b></p>
+
+```md
+[Expanses chart component | challenge] https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt
+```
+<img width="850px" src="https://user-images.githubusercontent.com/105606295/196008063-cffb363e-47b5-47e9-b551-98abe17f058c.png">
+
+<h2> 💻 Stacks utilizadas</h2>
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>Java Script</li>
+</ul>
+<h2> 💵 Licença</h2>
+<p><b>O uso de todos os arquivos deste projeto é limitado apenas a fins de estudos.<b></p>
---
+++ 05-Chart-component/assets/js/script.js
@@ -0,0 +1,65 @@
+/*Selecionando elementos no DOM*/
+const selectWeek = document.querySelector("#select-week")
+const chartTitle = document.querySelector("h2")
+const totalWeekSpent = document.querySelector(".total-week-spent > div")
+
+fetchAndAppend ()
+
+/*Buscando os dados em data.json*/
+async function fetchAndAppend (){
+    const data = await (await fetch("./data.json")).json()
+    appendChart(data["1stweek"])
+    setBarsHeight(data["1stweek"])
+    createWeekFilter(data)
+    addFilterListener(data)
+}
+/*Criando as colunas do gráfico*/
+function appendChart(data){
+    let components = data.reduce((acc, value)=>{
+        return acc+= `
+        <div class="day" id="${value.day}">
+            <div class="flex chart-data">
+                <div class="chart-bar"></div>
+                <div class="chart-label">$${value.amount}</div>
+            </div>
+            <span>${value.day}</span>
+        </div>`
+    },"")
+    document.querySelector(".chart").innerHTML = components
+}
+/*Adicionando altura das colunas e destaque para o maior valor*/
+function setBarsHeight(data){
+    let maxAmount = data.reduce((acc, value)=> {
+        if (acc < value.amount) acc = value.amount
+        return acc
+    }, 0)
+    for (let chartBar of document.querySelectorAll(".chart-bar")){
+        let amount = chartBar.nextElementSibling.innerHTML.slice(1)
+        chartBar.style.height = `${(amount*6/maxAmount).toFixed(1)}em`
+        maxAmount === Number(amount) ? chartBar.classList.add("biggest-bar") : null
+    }
+}
+
+/*Criando filtro de semanas e respectivo event.listener*/
+function createWeekFilter(data){
+    const options = Object.keys(data).reduce((acc, week)=>{
+        return acc+= `<option value="${week}">${week.slice(0,-4)} week</option>`
+    }, "")
+    selectWeek.innerHTML = options
+
+}
+function addFilterListener(data){
+    selectWeek.addEventListener("change",()=>{
+        const totalSpent = data[selectWeek.value].reduce((acc, value)=>acc+=value.amount, 0)
+        chartTitle.innerHTML = `Spending - ${selectWeek.value.slice(0,-4)} week`
+        totalWeekSpent.innerHTML = `$${totalSpent.toFixed(2)}`
+
+        document.querySelectorAll(".day").forEach((weekday, index)=>{
+            const chartBar = weekday.querySelector(".chart-bar")
+            chartBar.style.height = `0rem`
+            chartBar.classList[1] === "biggest-bar" ? chartBar.classList.remove("biggest-bar") : null
+            weekday.querySelector(".chart-label").innerHTML = `$${data[selectWeek.value][index].amount}`
+        })
+        setBarsHeight(data[selectWeek.value])
+    })  
+}

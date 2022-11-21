907700e0a0ca0327c887997f8bbefb3319753a02
Nalbert Cerqueira <nalbertc.p@gmail.com>
2022-11-21 19:25:03 -0300

    fixing prettier errors

--- 03-Notification-component/assets/js/script.js
+++ 03-Notification-component/assets/js/script.js
@@ -4,38 +4,37 @@ const noteCount = document.querySelector(".notifications-count")
 const elementsClass = ["profile-name", "additional-content", "note-img"]
 
 /*Adicionando event listeners aos elementos*/
-window.addEventListener("click", (event)=>{
+window.addEventListener("click", (event) => {
     if (event.target.className === "mark-as-read") readAll()
 })
-for (let notefication of notifications){
+for (let notefication of notifications) {
     notefication.addEventListener("click", notificationEvent)
 }
 
 /*Funções*/
-function notificationEvent(event){
+function notificationEvent(event) {
     let notification = this
     let targetClass = event.target.className
-    if (! elementsClass.every((value)=> value!=targetClass)){
+    if (!elementsClass.every((value) => value != targetClass)) {
         if (notification.classList.contains("read")) return event.stopPropagation()
         else return changeNotificationCount(notification)
     }
     changeNotificationCount(notification)
 }
-function changeNotificationCount(notification){
+function changeNotificationCount(notification) {
     markNotification(notification)
     if (notification.classList.contains("read")) noteCount.innerHTML = Number(noteCount.innerHTML) - 1
     else noteCount.innerHTML = Number(noteCount.innerHTML) + 1
 }
-function markNotification(notification){
+function markNotification(notification) {
     notification.classList.toggle("read")
     notification.querySelector(".unread-marker").classList.toggle("hidden")
 }
 
-function readAll(){
-    for (let notification of notifications){
+function readAll() {
+    for (let notification of notifications) {
         notification.classList.add("read")
         notification.querySelector(".unread-marker").classList.add("hidden")
     }
     noteCount.innerHTML = 0
 }
-

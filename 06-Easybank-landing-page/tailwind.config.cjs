1aa03ecbc25f6d7891c1ab9fa8ba7d37e47f760e
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-04-12 20:41:48 -0300

    Removed all unnecessary styles setted in tailwind.config.js.

--- 06-Easybank-landing-page/tailwind.config.cjs
+++ 06-Easybank-landing-page/tailwind.config.cjs
@@ -6,20 +6,6 @@ module.exports = {
             keyframes: {
                 "gradient-transition": {
                     "100%": { "background-position": "230%" }
-                },
-                "fade-menu": {
-                    "100%": { opacity: "0" }
-                },
-                "show-menu": {
-                    "0%": { opacity: "0" },
-                    "100%": { opacity: "1" }
-                },
-                "slide-up": {
-                    "100%": { transform: "translateY(-2.5rem)" }
-                },
-                "slide-down": {
-                    "0%": { transform: "translateY(-2.5rem)" },
-                    "100%": { transform: "translateY(0)" }
                 }
             },
             screens: {
